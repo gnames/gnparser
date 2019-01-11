ee0bb2aba8f835dbee46a685f0c41d9f5c092e32
Dmitry Mozzherin <dmozzherin@gmail.com>
2019-01-11 05:16:34 -0600

    hort and nom

--- gnparser_suite_test.go
+++ gnparser_suite_test.go
@@ -12,7 +12,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var testsNum = 458
+var testsNum = 474
 
 // TestGnparser is part of ``ginkgo`` package and is exposed because we want to
 // test some private libraries.
