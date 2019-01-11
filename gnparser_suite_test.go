dfb609c8e39d54672e023dd936b8b69be82a930d
Dmitry Mozzherin <dmozzherin@gmail.com>
2019-01-10 18:46:26 -0600

    cv cultivar

--- gnparser_suite_test.go
+++ gnparser_suite_test.go
@@ -12,7 +12,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var testsNum = 413
+var testsNum = 423
 
 // TestGnparser is part of ``ginkgo`` package and is exposed because we want to
 // test some private libraries.
