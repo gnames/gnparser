1b88cb4802ebaea62f57c929961f8b8858fca480
Dmitry Mozzherin <dmozzherin@gmail.com>
2019-01-11 05:25:02 -0600

    misc annot

--- gnparser_suite_test.go
+++ gnparser_suite_test.go
@@ -12,7 +12,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var testsNum = 474
+var testsNum = 483
 
 // TestGnparser is part of ``ginkgo`` package and is exposed because we want to
 // test some private libraries.
