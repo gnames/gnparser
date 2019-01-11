dea8b6bad230fe7992c9a7b4c70a2f3e98352122
Dmitry Mozzherin <dmozzherin@gmail.com>
2019-01-11 06:34:55 -0600

    Fix #20 make all tests pass (but html)

--- gnparser_suite_test.go
+++ gnparser_suite_test.go
@@ -12,7 +12,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var testsNum = 514
+var testsNum = 520
 
 // TestGnparser is part of ``ginkgo`` package and is exposed because we want to
 // test some private libraries.
